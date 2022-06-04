3628d3e25385d855f793dba603fb3aafd2ab84d4
Nikolay Gruychev <ngruychev@gmail.com>
2022-06-04 18:14:31 +0300

    chore: bump package version to 0.0.9

--- build_npm.ts
+++ build_npm.ts
@@ -8,7 +8,7 @@ import {
 } from "https://deno.land/std@0.140.0/path/mod.ts";
 import { copy } from "https://deno.land/std@0.140.0/fs/mod.ts";
 
-const VERSION = "0.0.8";
+const VERSION = "0.0.9";
 
 console.log(`Building version ${VERSION}`);
 
